8c281399adc73d31d6066c4551d092d5ffce5160
Emilio Velazco Pineda <emilio.velazcop@live.com.mx>
2019-01-14 11:42:42 -0600

    EVP
    Config database.php

--- config/database.php
+++ config/database.php
@@ -76,9 +76,9 @@ return [
         
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'localhost',
+            'host' => env('DB_HOST', '10.7.6.177'),
             'port' => env('DB_PORT', '3306'),
-            'database' => 'pcb_terceros',
+            'database' => env('DB_DATABASE', 'pes2018_prod'),
             'username' => env('DB_USERNAME', 'triada'), // triada
             'password' => 'eyJpdiI6Imx3YkNoWFAyMFpHUnNaNzdHbWcrOHc9PSIsInZhbHVlIjoiWVhhalwvaGxMbmVzcE5LWXRnY0xuY1E9PSIsIm1hYyI6IjcxNTUzN2M5ZTc5ZDJiNTdmODRmOGUxODIwNWVkN2ZkZDZlZDRmOGQzYzc0NzZhM2Q4Y2I2M2E0ZTUwNzM5YmIifQ==',
             'charset' => 'latin1',
