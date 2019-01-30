fbcc0d013f1f9d7d1f5f55ed02db132dab0b81af
Emilio Velazco Pineda <emilio.velazcop@live.com.mx>
2019-01-30 10:11:04 -0600

    EVP
    Corrección de consulta de autorizadores y responsables

--- app/Http/Controllers/tercerosController.php
+++ app/Http/Controllers/tercerosController.php
@@ -92,7 +92,7 @@ class tercerosController extends Controller
             $and=" AND `name` LIKE '%".$term."%'";
         }
         $sql="SELECT * FROM interface_labora ai
-                WHERE consecutive = (SELECT max(consecutive) FROM interface_labora) AND origen_id <> 999 $and LIMIT 5";
+                WHERE consecutive = (SELECT max(consecutive) FROM interface_labora AND origen_id <> 999) AND origen_id <> 999 $and LIMIT 5";
         $consultas = DB::select(DB::raw($sql));        
         $data=array();
         foreach ($consultas as $val) {
